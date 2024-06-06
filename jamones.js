38cc53a03e95553a5f02e11526f017ed21c5619a
EQ11A1\Juan Ra <coscolinguada@gmail.com>
2024-06-06 13:45:23 +0200

    A medio camino con la función "foreach"

---
+++ jamones.js
@@ -0,0 +1,24 @@
+Math.floor(Math.random() * 10001)
+
+cliente1 = "Jose Luis" + Math.floor(Math.random() * 10001)
+cliente2 = "Mariano" + Math.floor(Math.random() * 10001)
+cliente3 = "Chindasvinta" + Math.floor(Math.random() * 10001)
+cliente4 = "Calico" + Math.floor(Math.random() * 10001)
+cliente5 = "DonRamon" + Math.floor(Math.random() * 10001)
+cliente6 = "Alarico" + Math.floor(Math.random() * 10001)
+cliente7 = "Widerico" + Math.floor(Math.random() * 10001)
+cliente8 = "Perchita" + Math.floor(Math.random() * 10001)
+cliente9 = "SombraOscura" + Math.floor(Math.random() * 10001)
+cliente10 = "ChachoMigué" + Math.floor(Math.random() * 10001)
+listaclientes = [cliente1, cliente2, cliente3, cliente4, cliente5, cliente6, cliente7, cliente8, cliente9, cliente10]
+
+
+listaclientes.forEach(premios => {
+    
+    if(listaclientes[i] = 10000){
+        console.log(listaclientes[i] + "¡Enhorabuena, has ganado un jamón!")
+    }
+});
+    
+    
+
