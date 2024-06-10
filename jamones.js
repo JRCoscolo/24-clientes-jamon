12f9927cacdf4803189ca5ea721ead853584c168
EQ11A1\Juan Ra <coscolinguada@gmail.com>
2024-06-10 08:17:14 +0200

    Completado, funciona correctamente

--- jamones.js
+++ jamones.js
@@ -1,14 +1,13 @@
 function regalo() {
-    var valor;
+    
 
     for (cliente = 1; cliente <= 10; cliente++) {
-        valor = Math.floor(Math.random() * 15000);
-
+        let valor = Math.floor(Math.random() * 15000);
         switch (true) {
-            case valor > 10000 || valor < 15000:
+            case (valor >= 10000 && valor <= 15000):
                 console.log("cliente" + cliente + ":" + valor + "Gana cesta con jamón")
                 break;
-            case valor >= 1000 || valor < 10000:
+            case (valor >= 1000 && valor <= 10000):
                 console.log("cliente" + cliente + ":" + valor + "Gana cesta");
                 break;
             default:
